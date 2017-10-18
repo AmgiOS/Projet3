e5e0229cb406251900beb6d3b3fcfbbb94d21911
hakimamg <hakimdu9347@gmail.com>
2017-10-18 10:44:11 +0200

    Initial Commit

---
+++ Projet 3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Projet 3
+//
+//  Created by Amg on 18/10/2017.
+//  Copyright © 2017 Amg-Industries. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
